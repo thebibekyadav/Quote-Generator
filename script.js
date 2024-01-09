6521fa3e5946b7b948b8442e8c720155101456d5
Bibek Yadav <73220146+thebibekyadav@users.noreply.github.com>
2024-01-09 09:42:20 +0545

    Update script.js

--- script.js
+++ script.js
@@ -5,37 +5,42 @@ const quoteText = document.querySelector(".quotation"),
     copyBtn = document.querySelector(".copy"),
     twitterBtn = document.querySelector(".twitter"),
     synth = speechSynthesis;
+
 function randomQuote() {
     quoteBtn.classList.add("loading");
     quoteBtn.innerText = "Loading...";
-    fetch("http://api.quotable.io/random").then(response =>
+    fetch("https://api.quotable.io/random").then(response =>
         response.json()).then(result => {
             quoteText.innerText = result.content;
             authorName.innerText = result.author;
             quoteBtn.classList.remove("loading");
             quoteBtn.innerText = "New Quote";
+        })
+        .catch(error => {
+            console.error('Error fetching quote:', error);
+            quoteBtn.classList.remove("loading");
+            quoteBtn.innerText = "New Quote";
         });
 }
+
 speechBtn.addEventListener("click", () => {
     if (!quoteBtn.classList.contains("loading")) {
-        let utterance = new
-            SpeechSynthesisUtterance(`${quoteText.innerText} by
-${authorName.innerText}`);
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
         synth.speak(utterance);
-        setInterval(() => {
-            !synth.speaking ?
-                speechBtn.classList.remove("active") :
+        setInterval(() => {            !synth.speaking ?
+               speechBtn.classList.remove("active") :
                 speechBtn.classList.add("active");
         }, 10);
     }
 });
+
 copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(quoteText.innerText);
 });
+
 twitterBtn.addEventListener("click", () => {
-    let tweetUrl =
-        `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
     window.open(tweetUrl, "_blank");
 });
-quoteBtn.addEventListener("click", randomQuote);
 
+quoteBtn.addEventListener("click", randomQuote);
